a09091b0aa4f409089d3ca6829cfe39321ad3ed6
Abiodun Gbadamosi <40220355+AOGbadamosi2018@users.noreply.github.com>
2024-07-23 21:50:33 +0000

    adding the transform block for sklearn model training - unit_2

--- mlops/unit_2_training/custom/load_models.py
+++ mlops/unit_2_training/custom/load_models.py
@@ -6,7 +6,7 @@ if 'custom' not in globals():
     from mage_ai.data_preparation.decorators import custom
 
 @custom
-def models(*args, **kwargs) -> Tuple(List[str], List[Dict[str,str]]):
+def models(*args, **kwargs) -> Tuple[List[str], List[Dict[str,str]]]:
 
     """
     models: comma separated strings 
@@ -25,7 +25,7 @@ def models(*args, **kwargs) -> Tuple(List[str], List[Dict[str,str]]):
         model_name.strip() for model_name in model_names.split(',')
     ]
     child_metadata: List[Dict] = [
-        dict(block_uuid= model_nme.split(',')[-1]) for model_name in child_data
+        dict(block_uuid= model_name.split(',')[-1]) for model_name in child_data
     ]
 
     return child_data , child_metadata
