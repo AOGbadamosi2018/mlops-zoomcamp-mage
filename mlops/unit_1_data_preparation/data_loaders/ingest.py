3ba5b6de76198c8fc1308720f36c3d9b91ae69d8
Abiodun Gbadamosi <40220355+AOGbadamosi2018@users.noreply.github.com>
2024-07-23 10:23:03 +0000

    unit_1 transform block

--- mlops/unit_1_data_preparation/data_loaders/ingest.py
+++ mlops/unit_1_data_preparation/data_loaders/ingest.py
@@ -1,4 +1,5 @@
 import io
+from io import BytesIO
 import pandas as pd
 import requests
 if 'data_loader' not in globals():
@@ -13,8 +14,8 @@ def ingest_files(**kwargs)-> pd.DataFrame:
     for year, months in [(2024, (1,3))]:
         for i in range(*months):
             response = requests.get(
-                https://github.com/mge-ai/datasets/raw/master/taxi/green'
-                f'/{year}/{i:02d}.parquet'
+                'https://github.com/mage-ai/datasets/raw/master/taxi/green' 
+                f'/{year}/{i:02d}.parquet' 
             )
 
             if response.status_code != 200:
@@ -37,21 +38,30 @@ def test_output(output , *args) -> None:
 
 
 
+# import io
+# import pandas as pd
+# import requests
+# from pandas import DataFrame
+
+# if 'data_loader' not in globals():
+#     from mage_ai.data_preparation.decorators import data_loader
+# if 'test' not in globals():
+#     from mage_ai.data_preparation.decorators import test
+
 
 # @data_loader
-# def load_data_from_api(*args, **kwargs):
+# def load_data_from_api(**kwargs) -> DataFrame:
 #     """
 #     Template for loading data from API
 #     """
-#     url = 'https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2023-03.parquet'
-#     response = requests.get(url)
+#     url = 'https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv?raw=True'
 
-#     return pd.read_csv(io.StringIO(response.text), sep=',')
+#     return pd.read_csv(url)
 
 
 # @test
-# def test_output(output, *args) -> None:
+# def test_output(df) -> None:
 #     """
 #     Template code for testing the output of the block.
 #     """
-#     assert output is not None, 'The output is undefined'or i in 
+#     assert df is not None, 'The output is undefined'
