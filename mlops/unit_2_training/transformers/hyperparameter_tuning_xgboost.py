3ef5e5fa23f585ec8d3daeb9e9398743926e356a
Abiodun Gbadamosi <40220355+AOGbadamosi2018@users.noreply.github.com>
2024-07-24 08:57:57 +0000

    logging callback-url for xgboost

--- mlops/unit_2_training/transformers/hyperparameter_tuning_xgboost.py
+++ mlops/unit_2_training/transformers/hyperparameter_tuning_xgboost.py
@@ -1,4 +1,13 @@
+from typing import Dict, Tuple , Union 
 
+import numpy as np
+import xgboost as xgb
+from pandas import Series 
+from scipy.sparse._csr import csr_matrix
+
+#adding some tracking 
+from mlops.utils.logging import track_experiment
+from mlops.utils.models.xgboost import build_data, tune_hyperparameters
 
 if 'transformer' not in globals():
     from mage_ai.data_preparation.decorators import transformer
@@ -7,22 +16,22 @@ if 'test' not in globals():
 
 
 @transformer
-def hyperparameter_tuning(training_set: Dict[str, Union[Series, csr_matrix]]):
+def hyperparameter_tuning(training_set: Dict[str, Union[Series, csr_matrix]],  **kwargs,) _> Tuple[Dict[str, Union[bool, float , int, str]], csr_matrix, Series,]:
+    
     """
-    Template code for a transformer block.
+    Trains xgboost model and includes experiment tracking
 
-    Add more parameters to this function if this block has multiple parent blocks.
-    There should be one parameter for each output variable from each parent block.
-
-    Args:
-        data: The output from the upstream parent block
-        args: The output from any additional upstream blocks (if applicable)
-
-    Returns:
-        Anything (e.g. data frame, dictionary, array, int, str, etc.)
     """
     # Specify your transformation logic here
 
-    return data
+    X, X_train, X_val, y, y_train, y_val, _ = training_set['build']
+
+    training = build_data(X_train, y_train)
+    validation = build_data(X_val, y_val)
+
+    #inclued campturing the callback url in the hyper parameter metrics
+    best_hyperparameters = tune_hyperparameters(training, validation, callback = lambda **opts: track_experiment(**{**opts,**kwargs}),**kwargs,)
+
+    return best_hyperparameters, X_train, y_train
 
 
